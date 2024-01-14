811a881f92f9a5d2dcd4f3ccac46e31cacad7a3d
mertcansucu <mrtcnscc@gmail.com>
2024-01-14 17:33:19 +0300

    Models

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models;
 
 namespace ProductAPI.Controllers
 {
@@ -11,19 +12,32 @@ namespace ProductAPI.Controllers
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase
     {
-        private static readonly string[] Products ={//products listesi oluşturdum
-            "Iphone 14", "Iphone 15", "Iphone 16"
-        };
+        // private static readonly string[] Products ={//products listesi oluşturdum
+        //     "Iphone 14", "Iphone 15", "Iphone 16"
+        // };
+
+        //model üzerinden çağrıp bilgileri alıcam
+
+        private static List<Product>? _products;
+
+        public ProductsController(){
+            _products = new List<Product>{
+                new() { ProductId = 1, ProductName = "Iphone 14", Price = 60000, IsActive = true},
+                new() { ProductId = 2, ProductName = "Iphone 15", Price = 70000, IsActive = true},
+                new() { ProductId = 3, ProductName = "Iphone 16", Price = 80000, IsActive = true},
+                new() { ProductId = 4, ProductName = "Iphone 17", Price = 90000, IsActive = true}
+            };
+        }
 
         //localhost:5000/api/products => GET burda tüm listeyi
         [HttpGet]
-        public string[] GetProducts(){
-            return Products;
+        public List<Product> GetProducts(){
+            return _products ?? new List<Product>();//bunun kısayolu return _products == null ? new List<Product>() : _products
         }
 
         [HttpGet("{id}")]//localhost:5000/api/products/1 => GET ben burda sadece nir elemanı geri getiriyorum
-        public string GetProduct(int id){
-            return Products[id];
+        public Product GetProduct(int id){
+            return _products?.FirstOrDefault(i => i.ProductId == id) ?? new Product();//ben burda diyorum ki _products? null olmadığında içinde gez ve id si eşleşeni getir, ?? burda da diyorum ki eğer eşleşen yoksa null sa, new Product() boş olarak döndür(default değerleriyle)
         }
     }
 }
