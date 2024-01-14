2eb99418a1ec960f97c1028a55e5b28e81674f50
mertcansucu <mrtcnscc@gmail.com>
2024-01-14 18:18:25 +0300

    Ef Core Installation

---
+++ Models/ProductsContext.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductAPI.Models
+{
+    public class ProductsContext: DbContext
+    {
+        public ProductsContext(DbContextOptions<ProductsContext> options): base(options)
+        {
+            
+        }
+
+        //test verileri ekledim,datased içinde yapıyordum ama bunun basit yoluyla ekleme yaptım
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //HasData databasede yoksa eklenecek
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 1, ProductName = "Iphone 14", Price = 60000, IsActive = true});
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 2, ProductName = "Iphone 15", Price = 70000, IsActive = true});
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 3, ProductName = "Iphone 16", Price = 80000, IsActive = false});
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 4, ProductName = "Iphone 17", Price = 90000, IsActive = true});
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 5, ProductName = "Iphone 18", Price = 95000, IsActive = true});
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ProductsContext>(x => x.UseSqlite("Data Source = products.db"));
+/* bu işlemden sonra migrations oluşturucam, onun kodları
+    
+     dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 7.0.12
+    
+    dotnet add package Microsoft.EntityFrameworkCore.Design --version 7.0.12
+
+    dotnet build
+    dotnet tool update --global dotnet-ef --version 7.0.12 versiyonu güncelledim
+    dotnet ef migrations add InitialCreate
+    dotnet ef database update
+ 
+
+*/
 
 builder.Services.AddControllers();//yalın bir controller bu biz burda oluşturduğumuz json dataları frontend geliştirici alıp görüntüye çevirebilsin diye json dataları oluşturucaz. Bunu mobil içinde kullanılabilir alıp kullanabilirler. Ben burda sevice oluşturdum.
 //burdabki controller lar bir view ile bağlantılı olmayack yalın olarak oluşturucaz.Farklı uugulamlar arsındaki bağımlılığı kaldırmış olduk.
