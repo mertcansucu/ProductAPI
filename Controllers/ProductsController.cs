32becc1e54c19aec7437ba87fb34e79f87816e0b
mertcansucu <mrtcnscc@gmail.com>
2024-01-15 19:00:08 +0300

    Data Query

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProductAPI.Models;
 
 namespace ProductAPI.Controllers
@@ -18,32 +19,32 @@ namespace ProductAPI.Controllers
 
         //model üzerinden çağrıp bilgileri alıcam
 
-        private static List<Product>? _products;
+        private readonly ProductsContext _context;
 
-        public ProductsController(){
+        public ProductsController(ProductsContext context){
+            /*veritanından bilgialeri aldığım için sildim
             _products = new List<Product>{
                 new() { ProductId = 1, ProductName = "Iphone 14", Price = 60000, IsActive = true},
                 new() { ProductId = 2, ProductName = "Iphone 15", Price = 70000, IsActive = true},
                 new() { ProductId = 3, ProductName = "Iphone 16", Price = 80000, IsActive = true},
                 new() { ProductId = 4, ProductName = "Iphone 17", Price = 90000, IsActive = true}
             };
+            */
+            _context = context;
         }
 
         //localhost:5000/api/products => GET burda tüm listeyi
         [HttpGet]
-        public IActionResult GetProducts(){
+        public async Task<IActionResult> GetProducts(){
             // return _products ?? new List<Product>();//bunun kısayolu return _products == null ? new List<Product>() : _products
 
-            if (_products == null)
-            {
-                return NotFound();
-            }
+            var products =await _context.Products.ToListAsync();
 
-            return Ok(_products);
+            return Ok(products);
         }
 
         [HttpGet("{id}")]//localhost:5000/api/products/1 => GET ben burda sadece nir elemanı geri getiriyorum
-        public IActionResult GetProduct(int? id){
+        public async Task<IActionResult> GetProduct(int? id){
             /*ben artık dönderilen değerlein durumuna göre hata mesajları göndericem mesela ben kayıtlı olmasa bile 12 id li birini göderince default olarak dönderiyor ve durum olarak 200 yani başarılı dönderiyor ama bunun ben böyle olmasını istemiyorum eğer yoksa mesela hata mesajını vermem lazım ki frontend ci bunları anlayabilsin
              return _products?.FirstOrDefault(i => i.ProductId == id) ?? new Product();//ben burda diyorum ki _products? null olmadığında içinde gez ve id si eşleşeni getir, ?? burda da diyorum ki eğer eşleşen yoksa null sa, new Product() boş olarak döndür(default değerleriyle)
             */
@@ -53,7 +54,7 @@ namespace ProductAPI.Controllers
                 return NotFound();//burda hatayı dönderdim ama bunu kendim yazmak istersem şöyle yaparım return StatusCode(404,"")tırnaklar arsında istediğim meseajı yazıp cevabı dönderebilirim
             }
 
-            var p = _products?.FirstOrDefault(i => i.ProductId == id);//_products? böyle yaparak products listesinin boş olmadığından emin oldum eğer boş değilse aldım bilgileri
+            var p =await _context.Products.FirstOrDefaultAsync(i => i.ProductId == id);//_products? böyle yaparak products listesinin boş olmadığından emin oldum eğer boş değilse aldım bilgileri
 
             if (p == null)
             {
